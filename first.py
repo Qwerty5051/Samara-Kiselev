866cfd26feb29aeb292ddfc9ae7e658b917f7a6d
Smart developer <nik.kiselev.2010@inbox.ru>
2024-11-29 18:53:45 +0400

    111

--- first.py
+++ first.py
@@ -1,1 +1,28 @@
-print("My first git-repo")
+import pygame
+import random
+def draw_square(screen):
+    x, y = (random.randrange(0, 800), random.randrange(0, 600))
+    color = pygame.Color("#fbec5d")
+    # рисуем "тень"
+    pygame.draw.circle(screen, color, (x, y), random.randrange(1, 40))
+
+if __name__ == '__main__':
+    # инициализация Pygame:
+    pygame.init()
+    # размеры окна:
+    size = width, height = 800, 600
+    # screen — холст, на котором нужно рисовать:
+    screen = pygame.display.set_mode(size)
+
+    # формирование кадра:
+    # команды рисования на холсте
+    # ...
+    # ...
+    # смена (отрисовка) кадра:
+
+    # ожидание закрытия окна:
+    while pygame.event.wait().type != pygame.QUIT:
+        pygame.display.flip()
+        draw_square(screen)
+    # завершение работы:
+    pygame.quit()
